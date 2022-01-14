4b3656d23dac17f51ad1cd3c0c2aa551c16078ad
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-14 15:37:21 +0000

    Update ex01.c

--- ex01.c
+++ ex01.c
@@ -1,3 +1,15 @@
+1. chamar a biblioteca <unistd.h>
+2. chamar o int main e seus parametros (int argc, char *argv[])
+3. definir sobre a variavel i (int e igual a 0)
+4. definir as condicoes if (argc >= 2) e else que eh write(1, "a", 1)
+5. definir as condiceos while (if e break) e else que eh write (1, "\n", 1)
+6. em while, em if (argv[1][i] == "a") definir write(1, "\n", 1) e depois um break porque ja encontrou o a
+7. contador i++ dentro do while!!!
+8. cuidado com os colchetes.
+
+
+
+
 #include <unistd.h>
 
 int main(int argc, char *argv[])
